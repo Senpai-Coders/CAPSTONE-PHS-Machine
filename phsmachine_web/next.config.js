b7abbdde9c04bd9e5ee0c329cfab81c3e123f68f
Jervx <louellagracechua@gmail.com>
2022-08-24 17:54:52 +0800

    Fixed next config IP

--- phsmachine_web/next.config.js
+++ phsmachine_web/next.config.js
@@ -1,8 +1,26 @@
 /** @type {import('next').NextConfig} */
-import { GET_SERVER_IP } from "./helpers/api/index"
 
 const PI_IP = 11;
 const IS_PI = true;
+
+const GET_SERVER_IP = () => {
+    var interfaces = require("os").networkInterfaces();
+    for (var devName in interfaces) {
+      var iface = interfaces[devName];
+  
+      for (var i = 0; i < iface.length; i++) {
+        var alias = iface[i];
+        if (
+          alias.family === "IPv4" &&
+          alias.address !== "127.0.0.1" &&
+          !alias.internal
+        )
+          return alias.address;
+      }
+    }
+    return "0.0.0.0";
+  };
+
 const IP = GET_SERVER_IP()
 module.exports = {
   reactStrictMode: false,
