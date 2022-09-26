ec8b816e7b62b5a5ade7c72cf7b00c5cc69b1356
Jervx <louellagracechua@gmail.com>
2022-09-26 10:52:38 +0800

    Email only once per day when storage exceed 95%

--- phsmachine_web/pages/api/autoDelete.js
+++ phsmachine_web/pages/api/autoDelete.js
@@ -1,17 +1,16 @@
 import checkDiskSpace from "check-disk-space";
 import dbConnect from "../../configs/dbConnection";
 import NextCors from "nextjs-cors";
-const fs = require("fs");
 import logger from "../../services/logger";
-let ObjectId = require("mongoose").Types.ObjectId;
+import { PI_IP, dateToBeutify } from "../../helpers/api";
 
 const detections = require("../../models/thermal_detection");
+const configs = require("../../models/configs");
 
 dbConnect();
 
 const handler = async (req, res) => {
   await NextCors(req, res, {
-    // Options
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     origin: "*",
     optionsSuccessStatus: 200,
@@ -19,7 +18,7 @@ const handler = async (req, res) => {
 
   const storageInfo = await checkDiskSpace("/");
   const perc = ((storageInfo.size - storageInfo.free) / storageInfo.size) * 100;
-  const dets = await detections.find({}).sort({cat:1}).limit(1)
+  const dets = await detections.find({}).sort({ cat: 1 }).limit(1);
 
   var focPath = dets[0].img_normal;
   var fslash = focPath.indexOf("/", 1);
@@ -27,16 +26,61 @@ const handler = async (req, res) => {
   var delFold = focPath.substring(fslash + 1, sslash);
 
   if (perc >= 95.0) {
-    const update = await detections.deleteOne({ _id: new ObjectId(dets[0]._id) });
+    const update = await detections.deleteOne({
+      _id: new ObjectId(dets[0]._id),
+    });
     try {
-      const delRecrd = await fs.promises.rmdir(`public/detection/${delFold}`, { recursive: true });
-      logger.info(`Autodelte (exceed 95% free storage, deletion of old record must be done) -> Deleted detecion & it's data -> ${delFold}`);
+      // sendEmail Alert only today
+      var now = new Date();
+      var starting = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
+      const sentToday = await configs.find({
+        config_name: "storage_alert_email",
+        cat: { $gte: starting },
+      });
+
+      if (sentToday.length === 0) {
+        // send mail and create marker when last email
+        const deleteStorageMarkerEmail = await configs.deleteOne({
+          config_name: "storage_alert_email",
+        });
+
+        const createStorageMarkerEmail = await configs.create({
+          category: "config",
+          config_name: "storage_alert_email",
+          description:
+            "This specify or identify what was the last email sent for storage full alert",
+          value: new Date(),
+          disabled: false,
+          deletable: true,
+        });
+
+        const sendMails = await fetch(`http:${PI_IP}:3000/api/sendMail`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            type: 3,
+            template_content: {
+              time_string: dateToBeutify(new Date()),
+            },
+          }), 
+        });
+      }
+
+      const delRecrd = await fs.promises.rmdir(`public/detection/${delFold}`, {
+        recursive: true,
+      });
+      logger.info(
+        `Autodelte (exceed 95% free storage, deletion of old record must be done) -> Deleted detecion & it's data -> ${delFold}`
+      );
     } catch (e) {
       logger.error(`Failed To Delete Folder -> , ${e.stack}`);
     }
   }
 
-  res.status(200).json({ perc, canSave : perc < 95 });
+  res.status(200).json({ perc, canSave: perc < 95 });
 };
 
 export default handler;
