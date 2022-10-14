c251ad6e69819e661daf677bfc9aedae0802916f
Jervx <louellagracechua@gmail.com>
2022-10-14 14:15:52 +0800

    Make Sure Miniforge Works

--- Pig-Stress-DP-Algorithm/phs_system_runner.sh
+++ Pig-Stress-DP-Algorithm/phs_system_runner.sh
@@ -7,6 +7,7 @@ export TERM=xterm
 
 _EXPECTED_DIR="/home/$USER/CAPSTONE-PHS-Machine"
 _SYSTEM_DIR_="$_EXPECTED_DIR/Pig-Stress-DP-Algorithm"
+_EXPECTED_MINIFORGE_="/home/$USER/miniforge3/bin/python"
 
 if [ -d "$_EXPECTED_DIR" ]; then
   echo ""
@@ -15,6 +16,11 @@ else
     exit 0
 fi
 
-pip list
+if [ -d "$_EXPECTED_MINIFORGE_" ]; then
+  echo ""
+else
+    echo "Miniforge not installed or $_EXPECTED_MINIFORGE_ can't be located,  try running Installer-A.sh again.."
+    exit 0
+fi
 
 /home/$USER/miniforge3/bin/python "$_SYSTEM_DIR_/app.py"
