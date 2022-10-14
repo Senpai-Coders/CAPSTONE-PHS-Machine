acd8613967c4af6775565a717a379d8dc21a951d
Jervx <louellagracechua@gmail.com>
2022-10-14 20:24:59 +0800

    Change api key to .env.local

--- phsmachine_web/next.config.js
+++ phsmachine_web/next.config.js
@@ -35,6 +35,7 @@ module.exports = {
       : "mongodb+srv://Jervx:helloworld@capstone.nv1cu.mongodb.net/?retryWrites=true&w=majority",
     JWT_SCRT: "ErenJaeger",
     PI_IP : IP,
+    sendGrid : process.env.sendGrid,
     EXPIRATION: 86400, //Seconds
   },
 };
