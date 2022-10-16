a5671a615084d0455dc6a4ecfab6e0da9a02636f
Jervx <louellagracechua@gmail.com>
2022-10-16 14:43:51 +0800

    updated web runner sh

--- phsmachine_web/phs_web_runner.sh
+++ phsmachine_web/phs_web_runner.sh
@@ -1,11 +1,8 @@
-#! /bin/bash
 clear
-. ~/.nvm/nvm.sh
 . ~/.bashrc
+. ~/.config/nvm/nvm.sh
 
 _EXPECTED_DIR="/home/$USER/CAPSTONE-PHS-Machine"
 _PHS_WEB_DIR_="$_EXPECTED_DIR/phsmachine_web/"
-
-export NODE_ENV=development
 
 npm --prefix "$_PHS_WEB_DIR_" run start
