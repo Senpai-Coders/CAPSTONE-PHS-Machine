0dc7056d9e7c8cad17a1b1beff19adb3445e9785
Jervx <louellagracechua@gmail.com>
2022-10-16 14:17:49 +0800

    runner sh

--- phsmachine_docu/phs_docu_runner.sh
+++ phsmachine_docu/phs_docu_runner.sh
@@ -5,7 +5,7 @@ clear
 _EXPECTED_DIR="/home/$USER/CAPSTONE-PHS-Machine"
 _PHS_DOCU_DIR_="$_EXPECTED_DIR/phsmachine_docu/"
 
-cd "$_PHS_DOCU_DIR_/docs"
+cd "$_PHS_DOCU_DIR_"
 pwd
 
 python3 -m http.server 3001
