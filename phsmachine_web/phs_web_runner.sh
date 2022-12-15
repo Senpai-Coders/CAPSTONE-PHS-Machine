ea5f365942d8e17646d1f8012ba84f24b64bf7b8
Jervx <louellagracechua@gmail.com>
2022-12-15 22:22:26 +0800

    revert next config

--- phsmachine_web/phs_web_runner.sh
+++ phsmachine_web/phs_web_runner.sh
@@ -1,6 +1,9 @@
 clear
 . ~/.bashrc
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 _EXPECTED_DIR="/home/$USER/CAPSTONE-PHS-Machine"
 _PHS_WEB_DIR_="$_EXPECTED_DIR/phsmachine_web/"
 
