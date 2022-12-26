6711c7d6cec09340b8bc455c022ce110a594f342
Jervx <louellagracechua@gmail.com>
2022-12-26 13:49:36 +0800

    starter sh

--- phsmachine_web/phs_web_runner.sh
+++ phsmachine_web/phs_web_runner.sh
@@ -2,7 +2,7 @@ clear
 . ~/.bashrc
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 _EXPECTED_DIR="/home/$USER/CAPSTONE-PHS-Machine"
 _PHS_WEB_DIR_="$_EXPECTED_DIR/phsmachine_web"
@@ -25,29 +25,46 @@ curip=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
 lastbuild='-'
 
 while read line; do
-# reading each line
+    # reading each line
     lastbuild="$line"
-    n=$((n+1))
-done < $filename
+    n=$((n + 1))
+done <$filename
 
 echo "lastbuild :-> $lastbuild"
 echo -e "current_ip :-> $curip \n"
 
+max_rerun=5
+rerun_count=0
+
+build_phs() {
+    echo "building PHS"
+    npm --prefix "$_PHS_WEB_DIR_" run build
+    echo "$curip" | tee "$_PHS_WEB_DIR_/lastipbuild.tmp"
+}
+
+run_phs() {
+    npm --prefix "$_PHS_WEB_DIR_" run start
+    if [ $? -eq 1 ]; then
+        rerun_count=$(($rerun_count+1))
+        echo ""
+        echo ""
+        echo "FAILED TO RUN, retrying -> $rerun_count : max retry -> $max_rerun"
+        sleep 1
+        if [ $rerun_count -lt $max_rerun ]; then
+            build_phs
+            run_phs
+        fi
+    fi
+}
+
 sleep 1
 if [ "$lastbuild" != "$curip" ]; then
     echo "**need rebuild**"
-    echo "rebuilding PHS"
-    npm --prefix "$_PHS_WEB_DIR_" run build
-    echo "$curip" | tee "$_PHS_WEB_DIR_/lastipbuild.tmp"
+    build_phs
 else
     echo "**no need for rebuild**"
 fi
 
-if [ -d "$_PHS_WEB_DIR_/.next" ]; then
-    echo "**Build missing, rebuilding web**"
-    npm --prefix "$_PHS_WEB_DIR_" run build
-    echo "$curip" | tee "$_PHS_WEB_DIR_/lastipbuild.tmp"
-fi
-
 # npm --prefix "$_PHS_WEB_DIR_" run build
-npm --prefix "$_PHS_WEB_DIR_" run start
+run_phs
+echo "we tried $max_rerun times but the system won't start"
