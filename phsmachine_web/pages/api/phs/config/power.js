7996080e00b13c6fbf9950b89624b199277fa917
Jervx <louellagracechua@gmail.com>
2022-12-26 12:52:08 +0800

    shutdown lcd

--- phsmachine_web/pages/api/phs/config/power.js
+++ phsmachine_web/pages/api/phs/config/power.js
@@ -3,8 +3,7 @@ import logger from "../../../../services/logger";
 import { PI_IP } from "../../../../helpers/api";
 
 const handler = async (req, res) => {
-
-    const debs = true
+  const debs = true;
 
   try {
     const auth = req.cookies.authorization;
@@ -12,49 +11,54 @@ const handler = async (req, res) => {
     const { mode } = req.body;
 
     if (mode === 0) {
-      const response = fetch(`http://${PI_IP}:8000/shutdown_reboot?tostate=shutdown`, {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          tostate: "shutdown",
-        }),
-      })
+      const response = fetch(
+        `http://${PI_IP}:8000/shutdown_reboot?tostate=shutdown`,
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            tostate: "shutdown",
+          }),
+        }
+      )
         .then((response) => response.json())
         .then((data) => {})
         .finally(async () => {
           logger.info(
-            `User ${editorDetails.user_name}(${editorDetails._id}) -> Shutdown System`
+            `User ID : (${editorDetails._id}) executes -> Shutdown System`
           );
-          if(!debs){const shutdown = await exec_command("sudo shutdown now");}
+          if (!debs) {
+            const shutdown = await exec_command("sudo shutdown now");
+          }
         });
     } else if (mode === 1) {
-      const reboot = await exec_command("sudo reboot now");
-      logger.info(
-        `User ${editorDetails.user_name}(${editorDetails._id}) -> Restart System`
-      );
-
-      const response = fetch(`http://${PI_IP}:8000/shutdown_reboot?tostate=reboot`, {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          tostate: "reboot",
-        }),
-      })
+      const response = fetch(
+        `http://${PI_IP}:8000/shutdown_reboot?tostate=reboot`,
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            tostate: "reboot",
+          }),
+        }
+      )
         .then((response) => response.json())
         .then((data) => {})
         .finally(async () => {
           logger.info(
-            `User ${editorDetails.user_name}(${editorDetails._id}) -> Shutdown System`
+            `User ID : (${editorDetails._id}) executes -> Restart System`
           );
-          if(!debs){const shutdown = await exec_command("sudo shutdown now");}
+          if (!debs) {
+            const reboot = await exec_command("sudo reboot now");
+          }
         });
     }
     res.status(200).json({ message: "Done" });
